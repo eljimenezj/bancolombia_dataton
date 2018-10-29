c1438bcfb69216ddbae0e4ebb2385314f682463f
Juan Camilo Fernandez Prato <jcfernandezp@unal.edu.co>
2018-10-29 03:18:19 -0500

    lacked chat rules

--- pfm manager/server.R
+++ pfm manager/server.R
@@ -5,15 +5,29 @@ library(ggplot2)
 library(plotrix)
 library(knitr)
 library(plotly)
+require(smooth)
+library(data.table)
 
 # Cargamos datos
 
-fread('https://bancoblob.blob.core.windows.net/data/dt_info_pagadores_descriptivo.csv')
+#fread('https://bancoblob.blob.core.windows.net/data/dt_info_pagadores_descriptivo.csv')
 
 path <- "data/consolidado.csv"
+path2 <- "data/dt_info_pagadores_ingresos.csv"
+path3 <- "data/dt_info_pagadores_segmento.csv"
+path4 <- "data/dt_info_pagadores_descriptivo.csv"
 
 df <- fread(path,sep=",", header = T, stringsAsFactors = F,
             encoding="UTF-8")
+
+df2 <- fread(path2,sep=",", header = T, stringsAsFactors = F,
+             encoding="UTF-8")
+
+df3 <- fread(path3,sep=",", header = T, stringsAsFactors = F,
+             encoding="UTF-8")
+
+df4 <-  fread(path4,sep=",", header = T, stringsAsFactors = F,
+              encoding="UTF-8")
 
 colnames(df) <- c("id_trans","id_cliente","fecha","hora",
                   "valor_trx","ref1","ref2","ref3",
@@ -23,6 +37,73 @@ colnames(df) <- c("id_trans","id_cliente","fecha","hora",
 df$fecha <- as.Date(as.character(df$fecha), "%Y%m%d")
 
 server <- function(input, output, session) {
+    
+    output$segment <- renderValueBox({
+        id <- as.integer(input$id)
+        df_id <- df3[id_cliente==as.integer(id),]
+        valueBox(
+            value = df_id$segmento[1],
+            subtitle = "Segmento",
+            icon = icon("user", lib = "glyphicon"),
+            color = "green"
+        )
+    })
+    
+    output$monto <- renderValueBox({
+        id <- as.integer(input$id)
+        df_id <- df3[id_cliente==as.integer(id),]
+        valueBox(
+            value = paste0("$", formatC(as.numeric(df_id$monetary[1]), format="f", digits=2, big.mark=",")),
+            subtitle = "Monto",
+            icon = icon("usd", lib = "glyphicon"),
+            color = "blue"
+        )
+    })
+    
+    output$freq <- renderValueBox({
+        id <- as.integer(input$id)
+        df_id <- df3[id_cliente==as.integer(id),]
+        valueBox(
+            value = df_id$freq[1],
+            subtitle = "Frecuencia",
+            icon = icon("signal", lib = "glyphicon"),
+            color = "aqua"
+        )
+    })
+    
+    output$recen <- renderValueBox({
+        id <- as.integer(input$id)
+        df_id <- df3[id_cliente==as.integer(id),]
+        valueBox(
+            value = df_id$recency[1],
+            subtitle = "Recencia",
+            icon = icon("star", lib = "glyphicon"),
+            color = "yellow"
+        )
+    })
+    
+    output$consumo <- renderValueBox({
+        id <- as.integer(input$id)
+        df_id <- df3[id_cliente==as.integer(id),]
+        valueBox(
+            value = paste0("$", formatC(as.numeric(df_id$consumo_hist[1]), format="f", digits=2, big.mark=",")),
+            subtitle = "Consumo historico",
+            icon = icon("stats", lib = "glyphicon"),
+            color = "red"
+        )
+    })
+    
+    output$points <- renderValueBox({
+        id <- as.integer(input$id)
+        df_id <- df3[id_cliente==as.integer(id),]
+        valueBox(
+            value = df_id$total_point[1],
+            subtitle = "Puntos",
+            icon = icon("heart", lib = "glyphicon"),
+            color = "purple"
+        )
+    })
+    
     
     output$plot1 <- renderPlotly({
         id <- as.integer(input$id)
@@ -97,6 +178,192 @@ server <- function(input, output, session) {
         p10
     })
     
+    
+    # Saving 
+    
+    fc <- function(id){
+        df_id <- data.frame(valor_trx = df[id_cliente==id,]$valor_trx)
+        dfc   <- sma(df_id$valor_trx , h=3, silent=FALSE)
+        f     <- data.frame(valor_trx=as.numeric(dfc$forecast + (sd(df_id$valor_trx)*runif(1,0,0.1))*
+                                                     round(runif(1,-1,1),digits = 0)))
+        #return(rbind(df_id,f))
+        return(mean(f$valor_trx))
+    }
+    
+    fb <- function(id){
+        df_id     <- data.frame(valor_trx = df[id_cliente==id,]$valor_trx)
+        dfc       <- sma(df_id$valor_trx , h=3, silent=FALSE)
+        egreso    <- data.frame(valor_trx=as.numeric(dfc$forecast + (sd(df_id$valor_trx)*runif(1,0,0.1))*
+                                                         round(runif(1,-1,1),digits = 0)))
+        ingresos  <- data.frame(ingreso = df2[id_cliente==id,]$ingreso)
+        periodo   <- c(1:length(egreso$valor_trx))
+        df_plot   <- data.frame(periodo,ingresos,egreso)
+        return(df_plot)
+    }
+    
+    sc <- function(id,y){
+        df_id     <- data.frame(valor_trx = df[id_cliente==id,]$valor_trx)
+        dfc       <- sma(df_id$valor_trx , h=3, silent=FALSE)
+        ingresos  <- data.frame(ingreso = rep(df2[id_cliente==id,]$ingreso,3))
+        egreso    <- data.frame(valor_trx=as.numeric(dfc$forecast + (sd(df_id$valor_trx)*runif(1,0,0.1))*
+                                                         round(runif(1,-1,1),digits = 0)))
+        ahorro           <- data.frame(ahorro=(ingresos*y)-egreso)
+        colnames(ahorro) <- c("Ahorro")
+        return(ahorro)
+    }
+    
+    saving_capacity <- function(id,p_ahorro){
+        df_id     <- data.frame(valor_trx = df[id_cliente==id,]$valor_trx)
+        dfc       <- sma(df_id$valor_trx , h=3, silent=FALSE)
+        ingresos  <- data.frame(ingreso = df2[id_cliente==id,]$ingreso)
+        egreso    <- data.frame(valor_trx=as.numeric(dfc$forecast + (sd(df_id$valor_trx)*runif(1,0,0.1))*
+                                                         round(runif(1,-1,1),digits = 0)))
+        egreso    <- mean(egreso$valor_trx)
+        capacidad <- data.frame((ingresos*p_ahorro)-egreso)
+        colnames(capacidad) <- c("Monto_maximo")
+        return(capacidad) 
+    }
+    
+    prestamo_a_otorgar <- function(id, p_ahorro){
+        if(df4[id_cliente==id,]$seg_str == "OTRO"){
+            tasa <- 20
+            factor_mult <- 10.886508
+        }else if(df4[id_cliente==id,]$seg_str == "PERSONAL"){
+            tasa <- 18
+            factor_mult <- 10.983414
+        }else if(df4[id_cliente==id,]$seg_str == "PERSONAL PLUS"){
+            tasa <- 16
+            factor_mult <- 11.083126
+        }else if(df4[id_cliente==id,]$seg_str == "EMPRENDEDOR"){
+            tasa <- 14
+            factor_mult <- 11.185780
+        }else{ #(seg_str == "PREFERENCIAL")
+            tasa <- 12
+            factor_mult <- 11.291516
+        }
+        credito <- saving_capacity(id,p_ahorro)$Monto_maximo * factor_mult
+        credito <- ifelse(credito > 20000000, 20000000, credito)
+        
+        data.frame(Tasa = paste0(tasa,"%"," ","E.A"), Credito = credito)
+        
+    }
+    
+    lend_predict <- function(id){
+        
+        model <- readRDS("models/tree_model.rds")
+        
+        df4_1 <- copy(df4)
+        
+        df4_1[, id_cliente := as.integer(id_cliente)]
+        suppressWarnings(df4_1[, edad := as.integer(edad)])
+        edad_mean <- df4_1[, round(mean(edad, na.rm = T))]
+        df4_1[is.na(edad), edad := edad_mean]
+        df4_1[, seg_str := as.factor(seg_str)]
+        df4_1[, ocupacion := as.factor(ocupacion)]
+        df4_1[, tipo_vivienda := as.factor(tipo_vivienda)]
+        df4_1[, nivel_academico := as.factor(nivel_academico)]
+        df4_1[, estado_civil := as.factor(estado_civil)]
+        df4_1[, ingreso_rango := as.factor(ingreso_rango)]
+        
+        cliente <- df4_1[id_cliente == id]
+        if(nrow(cliente) == 0){
+            return(0)
+        }else{
+            predict.val <- predict(model, cliente)
+            return(ifelse((predict.val[, 2] < 0.7), 0, 1))
+        }
+    }
+    
+    output$plotcost <- renderPlotly({
+        id <- as.integer(input$id)
+        df_plot <- fb(id)
+        
+        # Grafica
+        print(df_plot)
+        scale_fac <- mean(df_plot$valor_trx)/min(df_plot$ingreso)
+        
+        p10 <- ggplot(df_plot, aes(x=periodo)) + 
+            geom_line(df_plot, y = valor_trx) + 
+            geom_line(df_plot, y = ingreso, aes(y=ingreso * scale_fac)) +
+            scale_y_continuous(sec.axis= sec_axis(~.*scale, name="Ingresos"))
+        
+        p10 <- p10 + ggtitle(paste("Ingresos estimados vs Pronóstico Gastos PSE \n Usuario ID: ",id))
+        p10 <- p10 + 
+            scale_y_continuous(labels = scales::comma, name = "Cifras en pesos COP")
+        
+        ggplotly(p10)
+    })
+    
+    output$month1 <- renderValueBox({
+        id <- as.integer(input$id)
+        df_plot <- sc(id, as.integer(input$saving_rate)/100)
+        valueBox(
+            value = paste0("$", formatC(as.numeric(df_plot$Ahorro[1]), format="f", digits=2, big.mark=",")),
+            subtitle = "Ahorro mes 1",
+            icon = icon("usd", lib = "glyphicon"),
+            color = if (df_plot$Ahorro[1] <= as.integer(input$meta)) "red" else "green"
+        )
+    })
+    
+    output$month2 <- renderValueBox({
+        id <- as.integer(input$id)
+        df_plot <- sc(id, as.integer(input$saving_rate)/100)
+        valueBox(
+            value = paste0("$", formatC(as.numeric(df_plot$Ahorro[1]), format="f", digits=2, big.mark=",")),
+            subtitle = "Ahorro mes 2",
+            icon = icon("usd", lib = "glyphicon"),
+            color = if (df_plot$Ahorro[2] <= as.integer(input$meta)) "red" else "green"
+        )
+    })
+    
+    output$month3 <- renderValueBox({
+        id <- as.integer(input$id)
+        df_plot <- sc(id, as.integer(input$saving_rate)/100)
+        valueBox(
+            value = paste0("$", formatC(as.numeric(df_plot$Ahorro[1]), format="f", digits=2, big.mark=",")),
+            subtitle = "Ahorro mes 3",
+            icon = icon("usd", lib = "glyphicon"),
+            color = if (df_plot$Ahorro[3] <= as.integer(input$meta)) "red" else "green"
+        )
+    })
+
+    # Credit 
+    
+    output$credit_lend <- reactive({
+        lend_predict(input$id)
+    })
+    
+    outputOptions(output, "credit_lend", suspendWhenHidden = FALSE) 
+    
+    output$rate <- renderValueBox({
+        amount <- prestamo_a_otorgar(input$id, as.integer(input$saving_rate)/100)
+        valueBox(
+            value = amount$Tasa[1],
+            subtitle = "Tasa",
+            icon = icon("ok", lib = "glyphicon"),
+            color = "green"
+        )
+    })
+    
+    output$amount <- renderValueBox({
+        amount <- prestamo_a_otorgar(input$id, as.integer(input$saving_rate)/100)
+        valueBox(
+            value = paste0("$", formatC(as.numeric(amount$Credito[1]), format="f", digits=2, big.mark=",")),
+            subtitle = "Monto",
+            icon = icon("usd", lib = "glyphicon"),
+            color = "green"
+        )
+    })
+    
+    output$periods <- renderValueBox({
+        valueBox(
+            value = 12,
+            subtitle = "Meses",
+            icon = icon("calendar", lib = "glyphicon"),
+            color = "green"
+        )
+    })
+    
     # Markdown download button
     
     output$downloadCert <- downloadHandler(
@@ -128,7 +395,7 @@ server <- function(input, output, session) {
         vars$user <- input$id
         vars$chat <- paste0(linePrefix(),
                             tags$span(class="username",
-                                      tags$abbr(title=Sys.time(), "Virtual Assistant")
+                                      tags$abbr(title=Sys.time(), "Asistente Virtual")
                             ),
                             ": ",
                             "Hola ", vars$user, "! En que te puedo ayudar?")
@@ -155,7 +422,7 @@ server <- function(input, output, session) {
                 
                 vars$chat <- c(vars$chat,  paste0(linePrefix(),
                                                   tags$span(class="username",
-                                                            tags$abbr(title=Sys.time(), "Virtual Assistant")
+                                                            tags$abbr(title=Sys.time(), "Asistente Virtual")
                                                   ),
                                                   ": ",
                                                   tagList(input$entry)))
@@ -182,7 +449,7 @@ server <- function(input, output, session) {
     
     # Messages
     
-    messageData <- data.frame(from = c('Virtual Advisor'), message = c('Invest your money here!'))
+    messageData <- data.frame(from = c('Asistente Virtual', 'Puntos Colombia'), message = c('Invierte tu dinero aqui!', 'Aumentar de segmento te da mas puntos colombia!'))
         
     output$messageMenu <- renderMenu({
         # Code to generate each of the messageItems here, in a list. This assumes
@@ -198,7 +465,7 @@ server <- function(input, output, session) {
     
     # Notifications
     
-    notifications <- data.frame(text=c('Expenses too high', 'Your credit has been aproved!'), 
+    notifications <- data.frame(text=c('Gastos muy altos', 'Tu credito ha sido aprobado!'), 
                                 icon=c('usd', 'ok'), 
                                 status=c('danger','success'))
     
@@ -213,7 +480,7 @@ server <- function(input, output, session) {
     
     # Tasks
     
-    el <- data.frame(value = c(95, 20), text = c('Saving for your motorcycle', 'Pay your mortage loan'))
+    el <- data.frame(value = c(95, 20), text = c('Ahorrando para tu moto!', 'Paga tu hipoteca'))
     el$color <- lapply(el$value, function(x){ifelse(x<30,"red",ifelse(x<80,"yellow","green"))})
     
     output$tasksMenu <- renderMenu({
--- pfm manager/ui.R
+++ pfm manager/ui.R
@@ -11,16 +11,16 @@ ui <- dashboardPage(
     dashboardSidebar(
         sidebarMenu(
             HTML('<center><img src="index.png" width="200" height="120"></center>'),
-            textInput("id", label = h4("Enter your ID:"), value = "12"),
+            textInput("id", label = h4("Ingresa tu ID:"), value = "12"),
             br(),
     
-            menuItem("Control your expenses", tabName = "expenses", icon = icon("briefcase", lib = "glyphicon")),
-            menuItem("Saving Capacity", tabName = "saving", icon = icon("piggy-bank", lib = "glyphicon")),
-            menuItem("Want a credit?", tabName = "credit", icon = icon("usd",lib = "glyphicon")),
-            menuItem("Invest your money!", tabName = "invest", icon = icon("btc", lib = "glyphicon")),
+            menuItem("Controla tus gastos", tabName = "expenses", icon = icon("briefcase", lib = "glyphicon")),
+            menuItem("Capacidad de ahorro", tabName = "saving", icon = icon("piggy-bank", lib = "glyphicon")),
+            menuItem("Quieres un credito?", tabName = "credit", icon = icon("usd",lib = "glyphicon")),
+            menuItem("Invierte tu dinero!", tabName = "invest", icon = icon("btc", lib = "glyphicon")),
             br(), br(),  
-            h5("App developed by The Plumbers "), HTML('<center><img src="plumbers.png" width="240" height="180"></center>'), h5(" for the Bancolombia's Dataton"),
-            h5("competition.")
+            h5("App desarrollada por The Plumbers "), HTML('<center><img src="plumbers.png" width="240" height="180"></center>'), h5(" para la competencia Dataton"),
+            h5("Bancolombia 2018")
         )
     ),
     dashboardBody(
@@ -28,6 +28,15 @@ ui <- dashboardPage(
             # First tab content
             tabItem(tabName = "expenses",
                     fluidRow(
+                        h2('Este es tu perfil!'),
+                        fluidRow(
+                            valueBoxOutput("segment"),
+                            valueBoxOutput("monto"),
+                            valueBoxOutput("freq"),
+                            valueBoxOutput("recen"),
+                            valueBoxOutput("consumo"),
+                            valueBoxOutput("points")
+                        ),
                         box(
                             h2('Como son sus gastos?'),
                             plotlyOutput("plot1", height = 250),
@@ -56,14 +65,38 @@ ui <- dashboardPage(
             
             # Second tab content
             tabItem(tabName = "saving",
-                    h2("Saving tab content")
+                    h2('Estos son tus estimados de ahorro!'),
+                    fluidRow(
+                        valueBoxOutput("month1"),
+                        valueBoxOutput("month2"),
+                        valueBoxOutput("month3")
+                    ),
+                    #
+                    div(style="display: inline-block;vertical-align:top; width: 250px;", numericInput("meta", h4("Ingresa tu meta de ahorro"), value = 500000)),
+                    div(style="display: inline-block;vertical-align:top; width: 500px;", numericInput("saving_rate", h4("Ingresa tu % de ahorro deseada"), value = 20, min=5, max=50)),
+                    h2('Observa tus ingresos y egresos'),
+                    plotlyOutput("plotcost", height = 250)
+                    
             ),
             
             # Third tab content
             tabItem(tabName = "credit",
-                    h2("Credit tab content"),
-                    actionButton("link", "Aprovecha tu credito", onclick ="window.open('https://www.grupobancolombia.com/wps/portal/personas')"),
-                    downloadButton("downloadCert", "Bajar certificado")
+                    conditionalPanel( condition = "output.credit_lend == 1",
+                                      h2("Enhorabuena! Tu credito esta aprobado! :)"),
+                                      h4('Debido a tu buen comportamiento y perfil con el banco, hemos decidido darte un credito a tasa preferencial.'),
+                                      valueBoxOutput("rate"),
+                                      valueBoxOutput("amount"),
+                                      valueBoxOutput("periods"),
+                                      h4('Puedes reclamar tu credito dando clic al boton "Aprovecha tu credito" o presentando el certificado en tu sucursal mas cercana.'),
+                                      actionButton("link", "Aprovecha tu credito", onclick ="window.open('https://www.grupobancolombia.com/wps/portal/personas')"),
+                                      downloadButton("downloadCert", "Descargar certificado")
+                                      ),
+                    conditionalPanel( condition = "output.credit_lend == 0",
+                                      h2("Lo sentimos! :("),
+                                      h4('En estos momentos no podemos otorgarte un credito rapido.'),
+                                      HTML('<center><img src="plumber-broken.png"></center>'),
+                                      h4('.')
+                                      )
             ),
             
             # Fourth tab content
@@ -84,7 +117,10 @@ ui <- dashboardPage(
                         div(actionButton("send", "Send")
                         )
                     )
-                )
+                ),
+                h2('Cuentanos tu experiencia con la app!'),
+                h4('No lo pienses dos veces! Puedes escribirnos tus inquietudes, problemas o agradecimientos y podras ganar Puntos Colombia! '), a("soporte@theplumbers.com",href=""),
+                h4('Para comunicarte con nosotros tambien puedes ingresar al siguiente link!'), a("PQRS Bancolombia",href="https://www.grupobancolombia.com/wps/portal/personas/contactanos/bancolombia")
             )
         )
     )
